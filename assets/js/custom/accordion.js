e30d55c59e9961375eac81f369a5778a3c435538
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-11 22:59:12 +0800

    Delete assets directory

--- assets/js/custom/accordion.js
+++
@@ -1,5 +0,0 @@
-$('.accordion-menu-trigger').click(function() {
-	var parent = $(this).parents('.accordion-menu-list__item'); // Find the parent item
-	parent.siblings().removeClass('open'); // Close other items
-	parent.toggleClass('open'); // Open parent item
-});
